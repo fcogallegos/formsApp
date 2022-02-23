206b8c2401aaca50e73ede7117cb4f93e81bfa9e
fcogallegos <fgallegosgo@gmail.com>
2022-02-23 12:29:14 -0700

    form submit

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,7 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
   styles: [
   ]
 })
-export class BasicsComponent {
+export class BasicsComponent implements OnInit {
 
 
   // myForm: FormGroup = new FormGroup({
@@ -17,16 +17,34 @@ export class BasicsComponent {
   // });
 
   myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.minLength(3) ] ],
-    price: [ 0, [ Validators.required, Validators.min(0) ] ],
-    stocks: [ 0, [ Validators.required, Validators.min(0) ] ] 
+    name: [ , [ Validators.required, Validators.minLength(3) ] ],
+    price: [ , [ Validators.required, Validators.min(0) ] ],
+    stocks: [ , [ Validators.required, Validators.min(0) ] ] 
   })
 
   constructor( private fb: FormBuilder ) { }
 
+  ngOnInit(): void {
+      this.myForm.reset({
+        name: 'RTX4080ti',
+        price: 1600,
+        stocks: 10
+      })
+  }
 
   fieldIsValid( field: string ) {
     return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  save() {
+    
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.myForm.value );
+    this.myForm.reset();
   }
 
 }
