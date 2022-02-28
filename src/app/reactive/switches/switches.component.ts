7566dd3cc924149618bd28e65eac5b2d7437cbb6
fcogallegos <fgallegosgo@gmail.com>
2022-02-28 12:51:18 -0700

    reactive forms: switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group ({
+    gender: [ 'M', Validators.required ],
+    notifications: [ true, Validators.required ],
+    terms: [ false, Validators.requiredTrue ]
+  });
+
+  person = {
+    gender: 'F',
+    notifications: true
+  }
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+      this.myForm.reset({
+          ...this.person,
+          terms: false
+      });
   }
 
 }
